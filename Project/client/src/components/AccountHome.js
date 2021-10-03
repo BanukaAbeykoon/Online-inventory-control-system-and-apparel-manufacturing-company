a5681a92aed7a3917ea61fb22545639e94cdb2f1
pathumHewage <it20185220@my.sliit.lk>
2021-10-04 01:49:00 +0530

    update insert

--- Project/client/src/components/AccountHome.js
+++ Project/client/src/components/AccountHome.js
@@ -190,7 +190,7 @@ export default class AccountHome extends Component {
                   <td>{account.other}</td>
                   <td>Rs {Number( account.sjournal)-(Number(account.pjournal)+Number(account.gjournal)) }</td>
 
-                  <td><a className = "btn btn-warning" href = {`/edit/${account._id}`}>
+                  <td><a className = "btn btn-warning" href = {`/editAcc/${account._id}`}>
                       <i className = "fas fa-edit"></i>&nbsp;Edit
                     </a>
                     &nbsp;
@@ -209,7 +209,7 @@ export default class AccountHome extends Component {
         
 
            
-            <button className="btn btn-success"><a href= "/add" style={{textDecoration:'none', color:'white'}}>
+            <button className="btn btn-success"><a href= "/addAcc" style={{textDecoration:'none', color:'white'}}>
             
               Create new </a> &nbsp;
               <i class="far fa-plus-square"></i>
