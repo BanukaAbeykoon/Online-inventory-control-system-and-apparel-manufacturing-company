d5d5458dae496d1997320b2b0f8c740e9db1bcec
DevshanL <it20109462@my.sliit.lk>
2021-10-06 11:37:23 +0530

    Create MatIns

---
+++ Project/client/src/components/MatIns.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MatIns extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
