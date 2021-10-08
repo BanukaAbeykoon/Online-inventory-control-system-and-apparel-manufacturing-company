41241c5a91f8cd4928e0843b909636e67214a103
L-Dhanushanka <it20244484@my.sliit.lk>
2021-10-08 14:15:45 +0530

    generate report

--- Project/client/src/components/RMReport.js
+++ Project/client/src/components/RMReport.js
@@ -1,5 +1,43 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+
+const generatePDF = (packing) => {
+  const doc = new jsPDF();
+  const tableColumn = [
+    "customer name",
+    "Order Id",
+    "Category",
+    "Payment",
+    "Quantity",
+    "Weight",
+    "Due Date",
+    "Address"
+  ];
+  const tableRows = [];
+
+  packing.map((packing) => {
+    const packingdata = [
+      packing.customer,
+      packing.orderId,
+      packing.category,
+      packing.payment,
+      packing.quantity,
+      packing.weight,
+      packing.dueDate,
+      packing.address,
+    ];
+    tableRows.push(packingdata);
+  });
+  doc.text("CASANOVA", 70, 8).setFontSize(13);
+  doc.text("Packing Details Report", 14, 16).setFontSize(13);
+  doc.autoTable(tableColumn, tableRows, {
+    styles: { fontSize: 8 },
+    startY: 35,
+  });
+  doc.save("Employee details.pdf");
+};
 
 export default class RMReport extends Component {
 
@@ -37,7 +75,6 @@ export default class RMReport extends Component {
         packing.orderId.toLowerCase().includes(searchKey)||
         packing.category.toLowerCase().includes(searchKey)||
         packing.payment.toLowerCase().includes(searchKey)||
-        packing.orderId.toLowerCase().includes(searchKey)||
         packing.quantity.toLowerCase().includes(searchKey)||
         packing.weight.toLowerCase().includes(searchKey)||
         packing.dueDate.toLowerCase().includes(searchKey)||
@@ -63,113 +100,75 @@ handleSearchArea = (e) =>{
 
     render() {
         return (
-            <div id="wrapper" className="toggled">
+          <div id="wrapper" className="toggled">
             <div id="page-content-wrapper">
-            <div className="container-fluid">
-      
-             
+              <div className="container-fluid">
                 <div className="row">
-      
                   <div className="col-lg-9 mt-2 mb-2">
                     <h4>Packing Detail Report</h4>
                   </div>
                   <div className=" col-lg-3 mt-2 mb-2">
-                  
                     <input
-      
-                    className="form-control"
-                    type="search"
-                    placeholder="Search..."
-                    name="searchQuery"
-                    onChange={this.handleSearchArea}>
-                      
-      
-                    </input>
-                   
-      
+                      className="form-control"
+                      type="search"
+                      placeholder="Search..."
+                      name="searchQuery"
+                      onChange={this.handleSearchArea}
+                    ></input>
                   </div>
                 </div>
-      
-            
-      
-      
-             
-               
-      
-                <table className ="table"> 
+
+                <table className="table">
                   <thead>
-      
                     <tr>
-               <th scope="col">#</th>
-              <th scope="col">Customer</th>
-              <th scope="col">OrderId</th>
-              <th scope="col">Category</th>
-              <th scope="col">Payment</th>
-              <th scope="col">Quantity</th>
-              <th scope="col">weight</th>
-              <th scope="col">DueDate</th>
-              <th scope="col">Address</th>
-                      
+                      <th scope="col">#</th>
+                      <th scope="col">Customer</th>
+                      <th scope="col">OrderId</th>
+                      <th scope="col">Category</th>
+                      <th scope="col">Payment</th>
+                      <th scope="col">Quantity</th>
+                      <th scope="col">weight</th>
+                      <th scope="col">DueDate</th>
+                      <th scope="col">Address</th>
                     </tr>
                   </thead>
                   <tbody>
-                   {this.state.packing.map((packing,index)=>(
+                    {this.state.packing.map((packing, index) => (
                       <tr key={index}>
-                        <th scope="row">{index+1}</th>
+                        <th scope="row">{index + 1}</th>
                         <td>
-                            <a href={`/packing/${packing._id}`} style={{textDecoration:'none'}}>
-                            {packing.orderId} 
-                            </a>
-                            </td>
-                <td>{packing.orderId}</td>
-                <td>{packing.category}</td>
-                <td>{packing.payment}</td>
-                <td>{packing.quantity}</td>
-                <td>{packing.weight}</td>
-                <td>{packing.dueDate}</td>
-                <td>{packing.address}</td>        
-                      </tr>  
-                        
-      
+                          <a
+                            href={`/packing/${packing._id}`}
+                            style={{ textDecoration: "none" }}
+                          >
+                            {packing.orderId}
+                          </a>
+                        </td>
+                        <td>{packing.orderId}</td>
+                        <td>{packing.category}</td>
+                        <td>{packing.payment}</td>
+                        <td>{packing.quantity}</td>
+                        <td>{packing.weight}</td>
+                        <td>{packing.dueDate}</td>
+                        <td>{packing.address}</td>
+                      </tr>
                     ))}
                   </tbody>
-            
-                  </table>
-      
-              
-      
-                 
-                  <button className="btn btn-success"><a href= "#" style={{textDecoration:'none', color:'white'}}>
-                  
-                    Download Report </a> &nbsp;
-                    <i class="fas fa-download"></i>
-      
+                </table>
+
+                <div>
+                  <button
+                    type="button"
+                    style={{ backgroundColor: "#2E4661", padding: "10px" }}
+                    class="btn btn-secondary btn-sm"
+                    onClick={() => generatePDF(this.state.packing)}
+                  >
+                    Report of Packing Form
                   </button>
-                 
-                  
-      
-      
+                </div>
               </div>
-              </div>
-
-
-              
-
-
-                  
- 
-
-
-                      
-
-
-
-
-
-
-
-              </div>
-              
-        )
+            </div>
+          </div>
+        );
     }
 }
